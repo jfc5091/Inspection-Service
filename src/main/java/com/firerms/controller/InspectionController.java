99393f837151b518d6dbf734155a9261b9c21787
jfc5091 <jfc5091@rit.edu>
2021-04-05 15:40:56 -0400

    finished InspectionService and InspectionController

--- src/main/java/com/firerms/controller/InspectionController.java
+++ src/main/java/com/firerms/controller/InspectionController.java
@@ -1,10 +1,17 @@
 package com.firerms.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.firerms.exception.EntityNotFoundException;
+import com.firerms.exception.EntityValidationException;
+import com.firerms.exception.IdNotNullException;
 import com.firerms.exception.InspectionExceptionHandler;
+import com.firerms.request.InspectionRequest;
+import com.firerms.response.InspectionResponse;
 import com.firerms.service.InspectionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/inspection")
@@ -15,4 +22,60 @@ public class InspectionController {
 
     @Autowired
     private InspectionExceptionHandler inspectionExceptionHandler;
+
+    @PostMapping("/create")
+    public ResponseEntity<Object> createInspection(@RequestBody InspectionRequest request) throws JsonProcessingException {
+        try {
+            InspectionResponse inspectionResponse = inspectionService.createInspection(request);
+            return ResponseEntity.status(HttpStatus.CREATED).body(inspectionResponse);
+        }
+        catch (IdNotNullException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.idNotNullException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.idNotNullException(e).getBody());
+        }
+        catch (EntityValidationException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityValidationException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityValidationException(e).getBody());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getInspection(@PathVariable("id") Long inspectionId) throws JsonProcessingException {
+        try {
+            InspectionResponse inspectionResponse = inspectionService.findInspectionById(inspectionId);
+            return ResponseEntity.status(HttpStatus.OK).body(inspectionResponse);
+        }
+        catch (EntityNotFoundException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityNotFoundException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityNotFoundException(e).getBody());
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Object> updateInspection(@RequestBody InspectionRequest request) throws JsonProcessingException {
+        try {
+            InspectionResponse inspectionResponse = inspectionService.updateInspection(request);
+            return ResponseEntity.status(HttpStatus.OK).body(inspectionResponse);
+        }
+        catch (EntityNotFoundException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityNotFoundException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityNotFoundException(e).getBody());
+        }
+        catch (EntityValidationException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityValidationException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityValidationException(e).getBody());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteInspection(@PathVariable("id") Long inspectionId) {
+        try {
+            inspectionService.deleteInspection(inspectionId);
+            return ResponseEntity.noContent().build();
+        }
+        catch (EntityNotFoundException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityNotFoundException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityNotFoundException(e).getBody());
+        }
+    }
 }
--- src/main/java/com/firerms/service/InspectionService.java
+++ src/main/java/com/firerms/service/InspectionService.java
@@ -1,7 +1,14 @@
 package com.firerms.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.firerms.amazon.s3.AmazonS3ClientServiceInterface;
+import com.firerms.entity.inspections.Inspection;
+import com.firerms.exception.EntityNotFoundException;
+import com.firerms.exception.IdNotNullException;
 import com.firerms.repository.InspectionRepository;
+import com.firerms.request.InspectionRequest;
+import com.firerms.response.InspectionResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +39,64 @@ public class InspectionService {
 
     private static final String NOT_FOUND_ERROR_MSG = "%s not found with id: %s";
     private static final String MUST_BE_NULL_ERROR_MSG = "id must be null for new %s";
-    private static final String VALIDATION_ERROR_MSG = "validation error: %s";
+
+    public InspectionResponse createInspection(InspectionRequest request) throws JsonProcessingException {
+        String requestString = new ObjectMapper().writeValueAsString(request);
+        LOG.info("Inspection Service - createInspection request: {}", requestString);
+        Inspection inspection = request.getInspection();
+        if (inspection.getInspectionId() != null) {
+            String errorMessage = String.format(MUST_BE_NULL_ERROR_MSG, "Inspection");
+            LOG.error("Inspection Service - createInspection request: {}", errorMessage);
+            throw new IdNotNullException(errorMessage);
+        }
+        inspection = inspectionRepository.save(inspection);
+        InspectionResponse inspectionResponse = new InspectionResponse(inspection);
+        String responseString = new ObjectMapper().writeValueAsString(inspectionResponse);
+        LOG.info("Inspection Service - successfully created Inspection: {}", responseString);
+        return inspectionResponse;
+    }
+
+    public InspectionResponse findInspectionById(Long inspectionId) throws JsonProcessingException {
+        LOG.info("Inspection Service - findInspectionById request: {}", inspectionId);
+        Inspection inspection = inspectionRepository.findByInspectionId(inspectionId);
+        if (inspection == null) {
+            String errorMessage = String.format(NOT_FOUND_ERROR_MSG, "Inspection", inspectionId);
+            LOG.error("Inspection Service - findInspectionById request: {}", errorMessage);
+            throw new EntityNotFoundException(errorMessage);
+        }
+        InspectionResponse inspectionResponse = new InspectionResponse(inspection);
+        String responseString = new ObjectMapper().writeValueAsString(inspectionResponse);
+        LOG.info("Inspection Service - successfully found Inspection: {}", responseString);
+        return inspectionResponse;
+    }
+
+    public InspectionResponse updateInspection(InspectionRequest request) throws JsonProcessingException {
+        String requestString = new ObjectMapper().writeValueAsString(request);
+        LOG.info("Inspection Service - updateInspection request: {}", requestString);
+        Inspection inspection = request.getInspection();
+        Long inspectionId = inspection.getInspectionId();
+        Inspection inspectionInDB = inspectionRepository.findByInspectionId(inspectionId);
+        if (inspectionInDB == null) {
+            String errorMessage = String.format(NOT_FOUND_ERROR_MSG, "Inspection", inspectionId);
+            LOG.error("Inspection Service - updateInspection request: {}", errorMessage);
+            throw new EntityNotFoundException(errorMessage);
+        }
+        inspection = inspectionRepository.save(inspection);
+        InspectionResponse inspectionResponse = new InspectionResponse(inspection);
+        String responseString = new ObjectMapper().writeValueAsString(inspectionResponse);
+        LOG.info("Inspection Service - successfully updated Inspection: {}", responseString);
+        return inspectionResponse;
+    }
+
+    public void deleteInspection(Long inspectionId) {
+        LOG.info("Inspection Service - deleteInspection request: {}", inspectionId);
+        Inspection inspection = inspectionRepository.findByInspectionId(inspectionId);
+        if (inspection == null) {
+            String errorMessage = String.format(NOT_FOUND_ERROR_MSG, "Inspection", inspectionId);
+            LOG.error("Inspection Service - deleteInspection request: {}", errorMessage);
+            throw new EntityNotFoundException(errorMessage);
+        }
+        inspectionRepository.delete(inspection);
+        LOG.info("Inspection Service - successfully deleted Inspection: {}", inspectionId);
+    }
 }
