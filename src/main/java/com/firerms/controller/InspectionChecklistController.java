a1fd630312f43f7a484b6b1795364f0085c71186
jfc5091 <jfc5091@rit.edu>
2021-04-05 15:37:43 -0400

    finished InspectionChecklistService and InspectionChecklistController

--- src/main/java/com/firerms/controller/InspectionChecklistController.java
+++ src/main/java/com/firerms/controller/InspectionChecklistController.java
@@ -1,10 +1,17 @@
 package com.firerms.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.firerms.exception.EntityNotFoundException;
+import com.firerms.exception.EntityValidationException;
+import com.firerms.exception.IdNotNullException;
 import com.firerms.exception.InspectionExceptionHandler;
+import com.firerms.request.InspectionChecklistRequest;
+import com.firerms.response.InspectionChecklistResponse;
 import com.firerms.service.InspectionChecklistService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/inspection/checklist")
@@ -15,4 +22,60 @@ public class InspectionChecklistController {
 
     @Autowired
     private InspectionExceptionHandler inspectionExceptionHandler;
+
+    @PostMapping("/create")
+    public ResponseEntity<Object> createInspectionChecklist(@RequestBody InspectionChecklistRequest request) throws JsonProcessingException {
+        try {
+            InspectionChecklistResponse inspectionChecklistResponse = inspectionChecklistService.createInspectionChecklist(request);
+            return ResponseEntity.status(HttpStatus.CREATED).body(inspectionChecklistResponse);
+        }
+        catch (IdNotNullException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.idNotNullException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.idNotNullException(e).getBody());
+        }
+        catch (EntityValidationException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityValidationException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityValidationException(e).getBody());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getInspectionChecklist(@PathVariable("id") Long inspectionChecklistId) throws JsonProcessingException {
+        try {
+            InspectionChecklistResponse inspectionChecklistResponse = inspectionChecklistService.findInspectionChecklistById(inspectionChecklistId);
+            return ResponseEntity.status(HttpStatus.OK).body(inspectionChecklistResponse);
+        }
+        catch (EntityNotFoundException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityNotFoundException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityNotFoundException(e).getBody());
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Object> updateInspectionChecklist(@RequestBody InspectionChecklistRequest request) throws JsonProcessingException {
+        try {
+            InspectionChecklistResponse inspectionChecklistResponse = inspectionChecklistService.updateInspectionChecklist(request);
+            return ResponseEntity.status(HttpStatus.OK).body(inspectionChecklistResponse);
+        }
+        catch (EntityNotFoundException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityNotFoundException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityNotFoundException(e).getBody());
+        }
+        catch (EntityValidationException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityValidationException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityValidationException(e).getBody());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteInspectionChecklist(@PathVariable("id") Long inspectionChecklistId) {
+        try {
+            inspectionChecklistService.deleteInspectionChecklist(inspectionChecklistId);
+            return ResponseEntity.noContent().build();
+        }
+        catch (EntityNotFoundException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityNotFoundException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityNotFoundException(e).getBody());
+        }
+    }
 }
--- src/main/java/com/firerms/service/InspectionChecklistService.java
+++ src/main/java/com/firerms/service/InspectionChecklistService.java
@@ -1,7 +1,14 @@
 package com.firerms.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.firerms.amazon.s3.AmazonS3ClientServiceInterface;
+import com.firerms.entity.checklists.InspectionChecklist;
+import com.firerms.exception.EntityNotFoundException;
+import com.firerms.exception.IdNotNullException;
 import com.firerms.repository.InspectionChecklistRepository;
+import com.firerms.request.InspectionChecklistRequest;
+import com.firerms.response.InspectionChecklistResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +39,64 @@ public class InspectionChecklistService {
 
     private static final String NOT_FOUND_ERROR_MSG = "%s not found with id: %s";
     private static final String MUST_BE_NULL_ERROR_MSG = "id must be null for new %s";
-    private static final String VALIDATION_ERROR_MSG = "validation error: %s";
+
+    public InspectionChecklistResponse createInspectionChecklist(InspectionChecklistRequest request) throws JsonProcessingException {
+        String requestString = new ObjectMapper().writeValueAsString(request);
+        LOG.info("Inspection Service - createInspectionChecklist request: {}", requestString);
+        InspectionChecklist inspectionChecklist = request.getInspectionChecklist();
+        if (inspectionChecklist.getInspectionChecklistId() != null) {
+            String errorMessage = String.format(MUST_BE_NULL_ERROR_MSG, "Inspection Checklist");
+            LOG.error("Inspection Service - createInspectionChecklist request: {}", errorMessage);
+            throw new IdNotNullException(errorMessage);
+        }
+        inspectionChecklist = inspectionChecklistRepository.save(inspectionChecklist);
+        InspectionChecklistResponse inspectionChecklistResponse = new InspectionChecklistResponse(inspectionChecklist);
+        String responseString = new ObjectMapper().writeValueAsString(inspectionChecklistResponse);
+        LOG.info("Inspection Service - successfully created Inspection Checklist: {}", responseString);
+        return inspectionChecklistResponse;
+    }
+
+    public InspectionChecklistResponse findInspectionChecklistById(Long inspectionChecklistId) throws JsonProcessingException {
+        LOG.info("Inspection Service - findInspectionChecklistById request: {}", inspectionChecklistId);
+        InspectionChecklist inspectionChecklist = inspectionChecklistRepository.findByInspectionChecklistId(inspectionChecklistId);
+        if (inspectionChecklist == null) {
+            String errorMessage = String.format(NOT_FOUND_ERROR_MSG, "Inspection Checklist", inspectionChecklistId);
+            LOG.error("Inspection Service - findInspectionChecklistById request: {}", errorMessage);
+            throw new EntityNotFoundException(errorMessage);
+        }
+        InspectionChecklistResponse inspectionChecklistResponse = new InspectionChecklistResponse(inspectionChecklist);
+        String responseString = new ObjectMapper().writeValueAsString(inspectionChecklistResponse);
+        LOG.info("Inspection Service - successfully found Inspection Checklist: {}", responseString);
+        return inspectionChecklistResponse;
+    }
+
+    public InspectionChecklistResponse updateInspectionChecklist(InspectionChecklistRequest request) throws JsonProcessingException {
+        String requestString = new ObjectMapper().writeValueAsString(request);
+        LOG.info("Inspection Service - updateInspectionChecklist request: {}", requestString);
+        InspectionChecklist inspectionChecklist = request.getInspectionChecklist();
+        Long inspectionChecklistId = inspectionChecklist.getInspectionChecklistId();
+        InspectionChecklist inspectionChecklistInDB = inspectionChecklistRepository.findByInspectionChecklistId(inspectionChecklistId);
+        if (inspectionChecklistInDB == null) {
+            String errorMessage = String.format(NOT_FOUND_ERROR_MSG, "Inspection Checklist", inspectionChecklistId);
+            LOG.error("Inspection Service - updateInspectionChecklist request: {}", errorMessage);
+            throw new EntityNotFoundException(errorMessage);
+        }
+        inspectionChecklist = inspectionChecklistRepository.save(inspectionChecklist);
+        InspectionChecklistResponse inspectionChecklistResponse = new InspectionChecklistResponse(inspectionChecklist);
+        String responseString = new ObjectMapper().writeValueAsString(inspectionChecklistResponse);
+        LOG.info("Inspection Service - successfully updated Inspection Checklist: {}", responseString);
+        return inspectionChecklistResponse;
+    }
+
+    public void deleteInspectionChecklist(Long inspectionChecklistId) {
+        LOG.info("Inspection Service - deleteInspectionChecklist request: {}", inspectionChecklistId);
+        InspectionChecklist inspectionChecklist = inspectionChecklistRepository.findByInspectionChecklistId(inspectionChecklistId);
+        if (inspectionChecklist == null) {
+            String errorMessage = String.format(NOT_FOUND_ERROR_MSG, "Inspection Checklist", inspectionChecklistId);
+            LOG.error("Inspection Service - deleteInspectionChecklist request: {}", errorMessage);
+            throw new EntityNotFoundException(errorMessage);
+        }
+        inspectionChecklistRepository.delete(inspectionChecklist);
+        LOG.info("Inspection Service - successfully deleted Inspection Checklist: {}", inspectionChecklistId);
+    }
 }
