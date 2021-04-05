02ffd8cb65a6c347ec6c33d0ace9e7fc7b07cf53
jfc5091 <jfc5091@rit.edu>
2021-04-05 15:43:11 -0400

    finished InspectionViolationService and InspectionViolationController

--- src/main/java/com/firerms/controller/InspectionViolationController.java
+++ src/main/java/com/firerms/controller/InspectionViolationController.java
@@ -1,10 +1,17 @@
 package com.firerms.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.firerms.exception.EntityNotFoundException;
+import com.firerms.exception.EntityValidationException;
+import com.firerms.exception.IdNotNullException;
 import com.firerms.exception.InspectionExceptionHandler;
+import com.firerms.request.InspectionViolationRequest;
+import com.firerms.response.InspectionViolationResponse;
 import com.firerms.service.InspectionViolationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/inspection/violation")
@@ -15,4 +22,60 @@ public class InspectionViolationController {
 
     @Autowired
     private InspectionExceptionHandler inspectionExceptionHandler;
+
+    @PostMapping("/create")
+    public ResponseEntity<Object> createInspectionViolation(@RequestBody InspectionViolationRequest request) throws JsonProcessingException {
+        try {
+            InspectionViolationResponse inspectionViolationResponse = inspectionViolationService.createInspectionViolation(request);
+            return ResponseEntity.status(HttpStatus.CREATED).body(inspectionViolationResponse);
+        }
+        catch (IdNotNullException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.idNotNullException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.idNotNullException(e).getBody());
+        }
+        catch (EntityValidationException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityValidationException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityValidationException(e).getBody());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getInspectionViolation(@PathVariable("id") Long inspectionViolationId) throws JsonProcessingException {
+        try {
+            InspectionViolationResponse inspectionViolationResponse = inspectionViolationService.findInspectionViolationById(inspectionViolationId);
+            return ResponseEntity.status(HttpStatus.OK).body(inspectionViolationResponse);
+        }
+        catch (EntityNotFoundException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityNotFoundException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityNotFoundException(e).getBody());
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Object> updateInspectionViolation(@RequestBody InspectionViolationRequest request) throws JsonProcessingException {
+        try {
+            InspectionViolationResponse inspectionViolationResponse = inspectionViolationService.updateInspectionViolation(request);
+            return ResponseEntity.status(HttpStatus.OK).body(inspectionViolationResponse);
+        }
+        catch (EntityNotFoundException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityNotFoundException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityNotFoundException(e).getBody());
+        }
+        catch (EntityValidationException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityValidationException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityValidationException(e).getBody());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteInspectionViolation(@PathVariable("id") Long inspectionViolationId) {
+        try {
+            inspectionViolationService.deleteInspectionViolation(inspectionViolationId);
+            return ResponseEntity.noContent().build();
+        }
+        catch (EntityNotFoundException e) {
+            return ResponseEntity.status(inspectionExceptionHandler.entityNotFoundException(e).getStatusCode())
+                    .body(inspectionExceptionHandler.entityNotFoundException(e).getBody());
+        }
+    }
 }
--- src/main/java/com/firerms/service/InspectionViolationService.java
+++ src/main/java/com/firerms/service/InspectionViolationService.java
@@ -1,7 +1,14 @@
 package com.firerms.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.firerms.amazon.s3.AmazonS3ClientServiceInterface;
+import com.firerms.entity.checklists.InspectionViolation;
+import com.firerms.exception.EntityNotFoundException;
+import com.firerms.exception.IdNotNullException;
 import com.firerms.repository.InspectionViolationRepository;
+import com.firerms.request.InspectionViolationRequest;
+import com.firerms.response.InspectionViolationResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +39,64 @@ public class InspectionViolationService {
 
     private static final String NOT_FOUND_ERROR_MSG = "%s not found with id: %s";
     private static final String MUST_BE_NULL_ERROR_MSG = "id must be null for new %s";
-    private static final String VALIDATION_ERROR_MSG = "validation error: %s";
+
+    public InspectionViolationResponse createInspectionViolation(InspectionViolationRequest request) throws JsonProcessingException {
+        String requestString = new ObjectMapper().writeValueAsString(request);
+        LOG.info("Inspection Service - createInspectionViolation request: {}", requestString);
+        InspectionViolation inspectionViolation = request.getInspectionViolation();
+        if (inspectionViolation.getInspectionViolationId() != null) {
+            String errorMessage = String.format(MUST_BE_NULL_ERROR_MSG, "Inspection Violation");
+            LOG.error("Inspection Service - createInspectionViolation request: {}", errorMessage);
+            throw new IdNotNullException(errorMessage);
+        }
+        inspectionViolation = inspectionViolationRepository.save(inspectionViolation);
+        InspectionViolationResponse inspectionViolationResponse = new InspectionViolationResponse(inspectionViolation);
+        String responseString = new ObjectMapper().writeValueAsString(inspectionViolationResponse);
+        LOG.info("Inspection Service - successfully created Inspection Violation: {}", responseString);
+        return inspectionViolationResponse;
+    }
+
+    public InspectionViolationResponse findInspectionViolationById(Long inspectionViolationId) throws JsonProcessingException {
+        LOG.info("Inspection Service - findInspectionViolationById request: {}", inspectionViolationId);
+        InspectionViolation inspectionViolation = inspectionViolationRepository.findByInspectionViolationId(inspectionViolationId);
+        if (inspectionViolation == null) {
+            String errorMessage = String.format(NOT_FOUND_ERROR_MSG, "Inspection Violation", inspectionViolationId);
+            LOG.error("Inspection Service - findInspectionViolationById request: {}", errorMessage);
+            throw new EntityNotFoundException(errorMessage);
+        }
+        InspectionViolationResponse inspectionViolationResponse = new InspectionViolationResponse(inspectionViolation);
+        String responseString = new ObjectMapper().writeValueAsString(inspectionViolationResponse);
+        LOG.info("Inspection Service - successfully found Inspection Violation: {}", responseString);
+        return inspectionViolationResponse;
+    }
+
+    public InspectionViolationResponse updateInspectionViolation(InspectionViolationRequest request) throws JsonProcessingException {
+        String requestString = new ObjectMapper().writeValueAsString(request);
+        LOG.info("Inspection Service - updateInspectionViolation request: {}", requestString);
+        InspectionViolation inspectionViolation = request.getInspectionViolation();
+        Long inspectionViolationId = inspectionViolation.getInspectionViolationId();
+        InspectionViolation inspectionViolationInDB = inspectionViolationRepository.findByInspectionViolationId(inspectionViolationId);
+        if (inspectionViolationInDB == null) {
+            String errorMessage = String.format(NOT_FOUND_ERROR_MSG, "Inspection Violation", inspectionViolationId);
+            LOG.error("Inspection Service - updateInspectionViolation request: {}", errorMessage);
+            throw new EntityNotFoundException(errorMessage);
+        }
+        inspectionViolation = inspectionViolationRepository.save(inspectionViolation);
+        InspectionViolationResponse inspectionViolationResponse = new InspectionViolationResponse(inspectionViolation);
+        String responseString = new ObjectMapper().writeValueAsString(inspectionViolationResponse);
+        LOG.info("Inspection Service - successfully updated Inspection Violation: {}", responseString);
+        return inspectionViolationResponse;
+    }
+
+    public void deleteInspectionViolation(Long inspectionViolationId) {
+        LOG.info("Inspection Service - deleteInspectionViolation request: {}", inspectionViolationId);
+        InspectionViolation inspectionViolation = inspectionViolationRepository.findByInspectionViolationId(inspectionViolationId);
+        if (inspectionViolation == null) {
+            String errorMessage = String.format(NOT_FOUND_ERROR_MSG, "Inspection Violation", inspectionViolationId);
+            LOG.error("Inspection Service - deleteInspectionViolation request: {}", errorMessage);
+            throw new EntityNotFoundException(errorMessage);
+        }
+        inspectionViolationRepository.delete(inspectionViolation);
+        LOG.info("Inspection Service - successfully deleted Inspection Violation: {}", inspectionViolationId);
+    }
 }
